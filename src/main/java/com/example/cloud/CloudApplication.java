1745e6ead140559fe1819816c59836035b7167c1
Thompsonclass <bbb7219@naver.com>
2024-12-02 13:26:54 +0900

    Hibernate 수정

--- src/main/java/com/example/cloud/CloudApplication.java
+++ src/main/java/com/example/cloud/CloudApplication.java
@@ -11,8 +11,8 @@ import com.example.cloud.repository.MovieRepository;
 @SpringBootApplication
 public class CloudApplication implements CommandLineRunner {
 
-    @Autowired
-    private MovieRepository movieRepository; // MovieRepository 사용
+    //@Autowired
+    //private MovieRepository movieRepository; // MovieRepository 사용
 
     public static void main(String[] args) {
         SpringApplication.run(CloudApplication.class, args);
@@ -33,8 +33,8 @@ public class CloudApplication implements CommandLineRunner {
         movie2.setSynopsis("영화 \"백두산\"은 한반도와 중국에 걸쳐 있는 백두산 화산의 폭발을 중심으로 한 재난 영화입니다. 백두산 화산이 갑작스럽게 폭발할 위험에 처하고, 이를 막기 위해 국가와 전 세계의 다양한 사람들이 협력하여 재난을 막으려는 과정을 그립니다.");
         movie2.setImageUrl("/a2.jpg");
 
-        movieRepository.save(movie1);
-        movieRepository.save(movie2);
+       //movieRepository.save(movie1);
+       //movieRepository.save(movie2);
 
         System.out.println("Initial data has been added to the database.");
     }
