765c67ad09e7a5c91f0d34284298e2748e77b618
Thompsonclass <bbb7219@naver.com>
2024-12-03 03:05:12 +0900

    revise test code

--- src/main/java/com/example/cloud/service/MovieService.java
+++ src/main/java/com/example/cloud/service/MovieService.java
@@ -3,7 +3,6 @@ package com.example.cloud.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
